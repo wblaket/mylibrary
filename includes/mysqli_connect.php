ef2f3b07a00deef2fb7315d9e40a861e3482eeba
Blake <wblaketharp@gmail.com>
2021-06-07 23:05:55 -0400

    updating styling

--- includes/mysqli_connect.php
+++ includes/mysqli_connect.php
@@ -4,10 +4,10 @@
 
 <?php
 
-  DEFINE ('DB_USER', '');
-  DEFINE ('DB_PASSWORD', '');
-  DEFINE ('DB_HOST', '');
-  DEFINE ('DB_NAME', '');
+  DEFINE ('DB_USER', 'root');
+  DEFINE ('DB_PASSWORD', 'Mchs2014!');
+  DEFINE ('DB_HOST', 'localhost');
+  DEFINE ('DB_NAME', 'mylibrary');
 
   $dbc = @mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die('Could not connect to MySQL: ' . mysqli_connect_error() );
 
